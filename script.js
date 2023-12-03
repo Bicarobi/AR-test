92d1c2ef1e3a7190dc4804b772402ebe056e96e8
Robert C <cmrecki.robert@gmail.com>
2023-12-03 21:57:29 +0100

    43

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 // We're going to register a custom event listener through a-frame that will fire
 // whenever a marker has entered the camera view and is found through ar.js
-/*$(document).ready(() => {
+$(document).ready(() => {
 	AFRAME.registerComponent("registerevents", {
 		init: function () {
 			var marker = this.el;
-
-			// Element emits events when found and lost
-			marker.setAttribute("emitevents", "true");
-
 			marker.addEventListener("markerFound", function () {
-				// Alright, a marker has been found. Let's get the video element
-				var vid = document.getElementById("waterVideo");
-
-				// Make sure that the video a-frame object is visible
-				document.querySelector("#water").setAttribute("visible", true);
-
-				// Reset the video to the beginning and play it through
-				vid.currentTime = 0;
-				vid.play();
+				var markerId = marker.id;
+				console.log("! markerFound", markerId);
+				// do additional things you want to do
+			});
+			marker.addEventListener("markerLost", function () {
+				var markerId = marker.id;
+				console.log("! markerLost", markerId);
+				// do additional things you want to do
 			});
 		},
 	});
-});*/
+});
